501e94649c5ced56aa3b55bbd318b13ffdc8be15
lesedi <analystmodikwe18@gmail.com>
2026-09-12 19:21:29 +0200

    adding rules

--- otp-security-system/backend/src/config.ts
+++ otp-security-system/backend/src/config.ts
@@ -1,6 +1,17 @@
+//  all OTP RULES
 export const OTP_CONFIG = {
-    MAX_OTP_PER_HOUR: 3,
-    OTP_EXPIRY_TIME : 30,
-    RESEND_WINDOW_MIN: 5,
-    MAX_RESEND_PER_OTP: 3
-}
+    MAX_OTP_PER_HOUR: Number(Process.env.OTP_MAX_REQUESTS_PER_HOUR ?? 3),
+
+
+    OTP_EXPIRY_TIME_SECONDS : Number(Process.env.OTP_EXPIRY_TIME_SECONDS ?? 30),
+
+
+    RESEND_WINDOW_MIN: Number(Process.env.OTP_RESEND_WINDOW_MINUTES ?? 5),
+
+
+    MAX_RESEND_PER_OTP: Number(ProcessingInstruction.env.OTP_MAX_RESENDS ?? 3),
+
+    CODE_LENGTH: 6,
+
+    HISTORY_WINDOW_HOURS: 24,
+};
