44c4759fad3b7f9e64d3b00d750cdecab4128ee5
lesedi <analystmodikwe18@gmail.com>
2026-09-14 21:07:44 +0200

    addding imports

--- otp-security-system/backend/src/otpService.ts
+++ otp-security-system/backend/src/otpService.ts
@@ -1,3 +1,23 @@
+// my routes will call this file
+
+import { OTP_CONFIG } from "./config"
+
+import {
+    getActiveOtp,
+    setActiveOtp,
+    getRecentHistory,
+    addToHistory,
+    getRequestTimestampsLastHour,
+    recordRequestTimestamp,
+} from "./otpStore";
+
+import {
+  OtpRecord,
+  CanRequestResult,
+  RequestOtpResult,
+  VerifyOtpResult,
+} from "./types";
+
 function canRequestOtp(email: string): { allowed: boolean; reason?: string }
 
 // this will loop against history internally
