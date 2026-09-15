19b388e3e190f5e0f0ead11b43fed10de48dbc01
lesedi <analystmodikwe18@gmail.com>
2026-09-15 07:59:45 +0200

    API

--- otp-security-system/frontend/src/pages/VerifyOtp.tsx
+++ otp-security-system/frontend/src/pages/VerifyOtp.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_BASE = "http://localhost:4000/api/otp";
+const API_BASE  = import.meta.env.VITE_API_BASE;
 
 export default function VerifyOtp() {
   const [email, setEmail] = useState("");
